0aa6150867c4be1b7ba16c546c2d87a9f73a06ee
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2024-04-17 13:33:56 +0100

    organize project layout

--- src/app/projects/[project]/page.tsx
+++ src/app/projects/[project]/page.tsx
@@ -1,5 +1,7 @@
+import ProjectParagraph from '@/components/projects/ProjectParagraph'
 import SectionTitle from '@/components/SectionTitle'
 import { projects } from '@/lib/projects'
+
 export default function ProjectPage({
   params,
 }: {
@@ -9,6 +11,29 @@ export default function ProjectPage({
   return (
     <section className="my-8 md:my-12">
       <SectionTitle title={project.name} />
+
+      <ProjectParagraph
+        img={project.mainImage}
+        imgType="desktop"
+        title="about"
+        text={project.description}
+      />
+      <ProjectParagraph
+        img={project.mobileImages[0]}
+        imgType="mobile"
+        text={project.description}
+      />
+      <ProjectParagraph
+        img={project.desktopImages[0]}
+        imgType="desktop"
+        text={project.description}
+      />
+      <ProjectParagraph
+        img={project.mobileImages[1]}
+        imgType="mobile"
+        title="results"
+        text={project.description}
+      />
     </section>
   )
 }
---
+++ src/components/projects/ProjectParagraph.tsx
@@ -0,0 +1,58 @@
+import clsx from 'clsx'
+import Image from 'next/image'
+
+import AnimationFadeIn from '../animation/AnimationFadeIn'
+
+export default function ProjectParagraph({
+  img,
+  imgType,
+  title,
+  text,
+}: {
+  img: string
+  imgType: 'desktop' | 'mobile'
+  title?: string
+  text: string
+}) {
+  return (
+    <div
+      className={clsx('flex flex-col gap-4', {
+        'my-12 lg:gap-12 lg:odd:flex-row-reverse lg:even:flex-row':
+          imgType === 'desktop',
+        'my-12 md:gap-12 md:odd:flex-row-reverse md:even:flex-row':
+          imgType === 'mobile',
+      })}
+    >
+      <AnimationFadeIn
+        styles={clsx('flex w-full justify-center', {
+          'lg:w-1/2': imgType === 'desktop',
+          'md:w-1/2': imgType === 'mobile',
+        })}
+      >
+        <Image
+          src={img}
+          alt="project-main-image"
+          width={imgType === 'desktop' ? 1200 : 400}
+          height={imgType === 'desktop' ? 625 : 800}
+          className={clsx('rounded-xl border-2 border-brand shadow-md', {
+            'w-full': imgType === 'desktop',
+            'max-h-[400px] w-auto md:max-h-[800px]': imgType === 'mobile',
+          })}
+        />
+      </AnimationFadeIn>
+      <AnimationFadeIn
+        styles={clsx('flex w-full flex-col justify-center self-start', {
+          'lg:mt-2 lg:w-1/2': imgType === 'desktop',
+          'md:mt-2 md:w-1/2': imgType === 'mobile',
+        })}
+      >
+        {title && (
+          <h2 className="text-3xl font-extrabold capitalize md:text-5xl">
+            {title}
+          </h2>
+        )}
+        <p className="mt-2 text-lg text-secondary md:text-2xl">{text}</p>
+      </AnimationFadeIn>
+    </div>
+  )
+}
